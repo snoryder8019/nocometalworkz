dfecbb27054a63d7c0db491942395f8d8c549a29
snoryder8019 <snoryder8019@gmail.com>
2022-10-12 12:50:54 -0600

    stable

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,40 @@
 const express = require('express');
 const passport = require('passport');
 const { ensureAuth } = require('../middleware/auth');
-
 const router = express.Router();
-
 
 router.get('/google', passport.authenticate(
   'google',
   {scope:['profile','email','openid']},
   {failureRedirect:'login'}),
-  // (req,res)=>{
-  //    res.redirect('/admin')}
-  )
+);
 
 router.get('/google/callback', passport.authenticate(
   'google',
    {failureRedirect:'login'}),
   (req,res)=>{
    res.redirect('/market')
-  })
+  }
+);
+
+// router.get('/auth/facebook',
+//   passport.authenticate('facebook',
+//   {scope:'profileFields'},
+//   {successRedirect:'/'},
+//   {failureRedirect:'/login'}
+//     )
+// );
+
+// router.get('/auth/facebook/callback',
+//   passport.authenticate('facebook', 
+//   {failureRedirect: '/login' }),
+//   function(req, res) {
+//     // Successful authentication, redirect home.
+//     res.redirect('/');
+//     }
+//   );
+
+
 
 
   router.get('/logout', function(req, res, next) {
