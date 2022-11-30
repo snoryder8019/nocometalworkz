96fbee1613d7a8e60e547ab05d0e82082ebff52c
snoryder8019 <snoryder8019@gmail.com>
2022-11-30 10:21:47 -0700

    err

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('config/404',{title:req.locals.error});
+  res.render('config/404',{title:err.message});
 });
 
 module.exports = app;
