8bbc0b279f0d93b6c3c94af53cbbf8e1ae57a3b1
snoryder8019 <snoryder8019@gmail.com>
2022-12-11 08:20:25 -0700

    added trust proxy

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ connectDB();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('trust proxy', 1)
+app.set('trust proxy', true)
 app.use(flash())
 app.use(logger('dev'));
 app.use(express.json());
