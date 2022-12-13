d1ed0731768cdb059989c5e8e737843573011a27
snoryder8019 <snoryder8019@gmail.com>
2022-12-13 14:02:31 -0700

    setup

--- routes/market.js
+++ routes/market.js
@@ -26,13 +26,13 @@ router.get('/market', (req,res) =>{
     //calling the function
     gettingBlogs().catch(console.error);
     async function getBlogs(client){
-     const user = req.session.user
-
+     const user = req.user
+console.log('user detected')
  const session = req.session.user
   const catagory = await client.db(dbName).collection('nm_catagories').find().toArray();
   const data = await client.db(dbName).collection('nm_inventory').find().toArray();
 if(req.user){
-  const cart = await client.db(dbName).collection('users').findOne({"_id":ObjectId(req.user._id)});
+  const cart = await client.db(dbName).collection('users').findOne({"_id":ObjectId(user._id)});
            //////total cart items
            const cartArray = []
            var cartTotal=0;    
