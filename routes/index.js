68648387ffa8d841282d6db47662cf4ed9f214f1
snoryder8019 <snoryder8019@gmail.com>
2022-12-15 14:59:20 -0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -78,7 +78,17 @@ res.render('index', {title:'Welcome',cartTotal:cartTotal,cart:cart,user:user, da
 });
 
 router.get('/faqs',(req,res)=>{
-  res.render('faqs')
+  async function faqFe(){
+    try{await client.connect()
+    await faqGetter(client)}
+    catch(err){console.log(err)}
+    finally{client.close()}
+  }
+  faqFe().catch(console.error);
+  async function faqGetter(client){
+const faqs = await client.db(dbName).collection('nm_faqs').find().toArray()
+res.render('faqs',{faqs:faqs})
+  }
 })
 router.get('/about',(req,res)=>{
   res.render('about')
