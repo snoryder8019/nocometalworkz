9e9b3728f1ba2f62d2a748577759e200b6325b7f
snoryder8019 <snoryder8019@gmail.com>
2022-12-16 08:59:25 -0700

    styl cart button

--- app.js
+++ app.js
@@ -1,38 +1,62 @@
-var createError = require('http-errors');
-var express = require('express');
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose')
+
+//dependancies
 const env = require('dotenv').config();
+const createError = require('http-errors');
 const connectDB = require('./config/db');
 const passport = require('./config/passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const bodyParser = require('body-parser')
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var url = require('url');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const url = require('url');
 const flash = require('express-flash')
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var registerRouter = require('./routes/register');
-var adminRouter =require('./routes/auth/admin');
-var visitorRouter =require('./routes/visitor');
-var apiRouter =require('./routes/api');
-var marketRouter =require('./routes/market');
+
+//views to individual pages
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const registerRouter = require('./routes/register');
+const adminRouter =require('./routes/auth/admin');
+const visitorRouter =require('./routes/visitor');
+const apiRouter =require('./routes/api');
+const marketRouter =require('./routes/market');
 const auth = require('./routes/auth');
-var app = express();
-connectDB();
+
+//setting template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs',{async:true});
+
+//MIDDLEWARES
+
+//hit DB before middlewares
+connectDB();
+//possible apache dependancies
 app.enable("trust proxy")
+
+//
+app.use(express.static(path.join(__dirname, 'public')));
+
+//parsing forms and responses to json
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+
+//messaging and logging
 app.use(flash())
 app.use(logger('dev'));
+
 app.use(express.json());
+//supprting https or mongo requests
 app.use(express.urlencoded({ extended: false }));
+
+//duh,,,, for cookies
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+
+///base passport js middleware //session options,login callbacks,requesting session
 app.use(session({
   secret:process.env.SESHID,
   resave:false,
@@ -42,7 +66,10 @@ cookie:{secure:true}
 }))
 app.use(passport.initialize())
 app.use(passport.session())
+
+//setting an authorized route for admins
 app.use("/auth",auth);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(registerRouter);
@@ -58,7 +85,7 @@ app.use((req,res,next)=>{
 app.use(function(req, res, next) {
   next(createError(404));
 
-  res.render('config/404', {title:'Hmmmmmm....somethings not straight here....Error:404'});
+  res.render('config/404', {title:err});
 });
 
 // error handler
